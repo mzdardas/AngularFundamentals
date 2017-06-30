e3028b161f12895350dc984e153676c62f185475
Marios Z Dardas <mzdardas@users.noreply.github.com>
2017-06-30 16:06:10 -0400

    Update to landing page

--- app/js/controllers/EventController.js
+++ app/js/controllers/EventController.js
@@ -12,11 +12,41 @@ var EventController = function($scope){
         },
         imageUrl: '/img/angularjs-logo.png',
         sessions: [
-            {name: 'Directives Masterclass'},
-            {name: 'Scopes for fun and profit'},
-            {name: 'Well Behaved Controllers'}
+            {
+                name: 'Directives Masterclass',
+                creatorName:'Bob Smith',
+                duration:'1 hr',
+                level:'Advanced',
+                abstract:'In this session you will learn the ins and outs of directives!',
+                upVoteCount: 0
+            },
+            {
+                name: 'Scopes for fun and profit',
+                creatorName:'John Doe',
+                duration:'30 mins',
+                level:'Introductory',
+                abstract:'This session will look closer at scopes. Learn what they do, etc...',
+                upVoteCount: 0
+            },
+            {
+                name: 'Well Behaved Controllers',
+                creatorName:'Jane Doe',
+                duration:'2 hrs',
+                level:'Intermediate',
+                abstract:'Controllers are essential components that allow Angular to do what it does',
+                upVoteCount: 0
+            }
         ]
     }
+
+    $scope.upVoteSession = function(session){
+        session.upVoteCount++;
+    }
+
+    $scope.downVoteSession = function(session){
+        session.upVoteCount--;
+    }
+
 };
 
 eventsApp.controller('EventController', ["$scope", EventController]);
